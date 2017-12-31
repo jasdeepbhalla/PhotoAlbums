37d62d9ee81aefaacec24c4d6ffd0614abc439ba
jasdeepbhalla <jasdeepbhalla@gmail.com>
2017-12-31 23:30:27 +0530

    adding components

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry, Text, View } from 'react-native';
+import React from 'react';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList'
+
+const App = () => (
+  <View>
+    <Header headerText={'Albums'}/>
+    <AlbumList />
+  </View>
+)
+
 
 AppRegistry.registerComponent('PhotoAlbums', () => App);
